693bc398175f49ad47aff9795fed8b98ff01e6bd
evgenicode <83199154+evgenicode@users.noreply.github.com>
2024-11-12 23:42:26 +0200

    add player camera limit script

--- levels/level_base.gd
+++ levels/level_base.gd
@@ -3,3 +3,10 @@ extends Node2D
 func _ready():
 	$Items.hide()
 	$Player.reset($SpawnPoint.position)
+	set_camera_limits()
+	
+func set_camera_limits():
+	var map_size = $World.get_used_rect()
+	var cell_size = $World.tile_set.tile_size
+	$Player/Camera2D.limit_left = (map_size.position.x - 5) * cell_size.x
+	$Player/Camera2D.limit_right = (map_size.end.x + 5) * cell_size.x
